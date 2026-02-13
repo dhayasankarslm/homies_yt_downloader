318504d81beb74d572c08e81f5a6f20f5c15ade9
dhayasankar <dhayasankarslm@gmail.com>
2026-02-13 04:17:50 +0100

    Discord webhook

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from PySide6.QtWidgets import QApplication
 from ui.media_app import MediaApp
 from ui.splash_screen import SplashScreen
 
+#testing out discord commit message webhook integration.
+
 os.environ["PATH"] += os.pathsep + "ffmpeg"
 
 if __name__ == "__main__":
